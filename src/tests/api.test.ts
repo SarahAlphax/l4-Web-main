38ce244a148eeea8e052f68216e8b04fa7539558
Sarah <sarahelsakt@gmail.com>
2025-06-16 14:47:53 +0300

    sdf

---
+++ src/tests/api.test.ts
@@ -0,0 +1,11 @@
+// describe('Example test', () => {
+//   it('should run properly', () => {
+//     expect(true).toBeTrue();
+//   });
+// });
+
+describe('Example test', () => {
+  it('should run properly', () => {
+    expect(true).toBe(true);
+  });
+});
